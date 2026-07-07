c607f64df859048a4a3e38493e8dcf068da97afd
Damandeep Kour <daman1901319@gmail.com>
2026-07-07 17:39:02 +0530

    new apis for getAIFeedback and getAIFeedbackDetails.

--- src/Controllers/feedbackController.js
+++ src/Controllers/feedbackController.js
@@ -22,3 +22,22 @@ export const getAIFeedback = async (req, res) => {
   //     newFeedback,
   //   });
   // };
+
+  export const getAIFeedbackList = async (req, res) => {
+    const feedbackList = await getAIFeedbackListService();
+
+    res.json({
+      success: true,
+      feedbackList,
+    });
+  };
+
+  export const getAIFeedbackDetail = async (req, res) => {
+    const { id } = req.params;
+    const feedbackDetail = await getAIFeedbackDetailService(id);
+
+    res.json({
+      success: true,
+      feedbackDetail,
+    });
+  };
