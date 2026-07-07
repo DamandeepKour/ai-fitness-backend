50fd8f7e1b0d8ad6dc078327be2f60cd3885a309
Damandeep Kour <daman1901319@gmail.com>
2026-07-07 17:40:03 +0530

    changes in feeback controller to comment the apis whoch is not in use for frontend.

--- src/Controllers/feedbackController.js
+++ src/Controllers/feedbackController.js
@@ -23,21 +23,21 @@ export const getAIFeedback = async (req, res) => {
   //   });
   // };
 
-  export const getAIFeedbackList = async (req, res) => {
-    const feedbackList = await getAIFeedbackListService();
+  // export const getAIFeedbackList = async (req, res) => {
+  //   const feedbackList = await getAIFeedbackListService();
 
-    res.json({
-      success: true,
-      feedbackList,
-    });
-  };
+  //   res.json({
+  //     success: true,
+  //     feedbackList,
+  //   });
+  // };
 
-  export const getAIFeedbackDetail = async (req, res) => {
-    const { id } = req.params;
-    const feedbackDetail = await getAIFeedbackDetailService(id);
+  // export const getAIFeedbackDetail = async (req, res) => {
+  //   const { id } = req.params;
+  //   const feedbackDetail = await getAIFeedbackDetailService(id);
 
-    res.json({
-      success: true,
-      feedbackDetail,
-    });
-  };
+  //   res.json({
+  //     success: true,
+  //     feedbackDetail,
+  //   });
+  // };
